988e83af96d08d2565777a3adc12fa700aff9e00
aptosfomo <167626275+aptosfomo@users.noreply.github.com>
2024-04-21 20:58:33 +0300

    Almost done

--- assets/js/abitharderthistime.js
+++ assets/js/abitharderthistime.js
@@ -2,7 +2,8 @@ $('.omni').click(function() {
   const well = $('<div></div>').addClass("modal fade")
   const done = $('<div></div>').addClass("modal-dialog modal-fullscreen")
 
-  const share = $('<div></div>').attr("data-bs-dismiss", "modal").addClass("modal-content border-0").css("background-color", "#dab692")
+  const share = $('<div></div>').attr("data-bs-dismiss", "modal").addClass("modal-content border-0").css("background-image", "url(./assets/images/bg.webp)").css("background-size", "cover").css("background-position", "center")
+
   const screenshot = $('<div></div>').addClass("modal-body text-center")
   const on = $('<img>').css("max-width", "500px").attr("src","./assets/images/eet.svg")
   const discord = $('<h1>Aptos Fomo</h1>').addClass("text-black")
